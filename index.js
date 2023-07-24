403246c593da2fed7225b4278f011f1d8a78877c
JLopez1227 <josuenmilopez27@gmail.com>
2023-07-24 12:37:40 -0500

    Got a basic setup to generate README.

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+const fs = require ('fs')
+const inquirer = require ('inquirer')
+const generateMarkdown = require ('./utils/generateMarkdown')
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your README?',
+        name: 'title'
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then( function (answers){
+        console.log(answers)
+        var markDown = generateMarkdown(answers)
+        console.log(markDown)
+        fs.writeFileSync('README2.md', markDown)
+    })
+
+}
+
+// Function call to initialize app
+init();
